1f9078470575ed715f9ab9c7fc2afc4f1c159908
Casper Panduro <cp@codebypanduro.dk>
2023-10-05 23:14:09 +0200

    cleaner functions.php

--- functions.php
+++ functions.php
@@ -1,15 +1,11 @@
 <?php
-use CodeByPanduro\Theme\Setup;
-
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/app/theme.php';
 
-function setup_theme() {
+add_action('after_setup_theme', function() {
     theme()
         ->headlessSetup();
-}
-
-add_action('after_setup_theme', 'setup_theme');
+});
 
 // register custom post types
 add_action('init', function() {
